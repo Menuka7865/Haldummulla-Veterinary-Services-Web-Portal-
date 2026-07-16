74f5d30fbbc1356a3e99c3c1f6cd1c0d7fb1689c
Kaumadii <162365196+Kaumadii@users.noreply.github.com>
2026-07-16 11:34:24 +0530

    feat: add Announcements, Login, and Register pages

--- frontend/web-portal/src/App.jsx
+++ frontend/web-portal/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home.jsx'
 import Services from './pages/Services.jsx'
+import Announcements from './pages/Announcements.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
 
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/services" element={<Services/>}/>
+        <Route path="/announcements" element={<Announcements/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
 
         
       </Routes>
